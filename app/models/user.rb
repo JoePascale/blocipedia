b114c01570e76a88717390e28b367de7bdd3b8c8
JoePascale <joe.m.pascale@gmail.com>
2017-09-13 10:51:37 -0700

    installed pundit gem, set default user role as standard

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :wikis
+
+  after_initialize :default
+
+  def default
+    #self.role ||= standard
+  end
 end
