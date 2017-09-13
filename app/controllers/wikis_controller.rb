fb3dd7d4cc19a4753a6aef9be8b012c1e1ee450a
JoePascale <joe.m.pascale@gmail.com>
2017-09-12 21:46:33 -0700

    completed wikis controller with create, update, & destroy methods now finished

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -16,8 +16,7 @@ class WikisController < ApplicationController
   end
 
   def create
-    @wiki = Wiki.build
-    #@wiki.user = current_user
+    @wiki = current_user.wikis.new(wiki_params)
 
     if @wiki.save
       flash[:notice] = "Wiki was saved."
@@ -26,5 +25,37 @@ class WikisController < ApplicationController
       flash.now[:alert] = "There was an error saving the wiki. Please try again."
       render :new
     end
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Post was updated."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "There was an error deleting the wiki."
+      render :show
+    end
+  end
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
   end
 end
