ac15ddb423aee9ba85e87c22e81d4a3550150111
JoePascale <joe.m.pascale@gmail.com>
2017-09-13 19:48:17 -0700

    seeded db with fake data from Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create{email: 'joe.m.pascale@gmail.com', Password: 'helloworld', confirmed_at: Time.now}
+#User.create({email: 'joe.m.pascale@gmail.com', Password: 'helloworld', confirmed_at: Time.now})
+
+50.times do
+  User.create([{
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    role: Faker::Number.between(0, 2)
+  }])
+end
+
+50.times do
+  Wiki.create([{
+    title: Faker::Lorem.word,
+    body: Faker::Lorem.paragraph
+  }])
+end
