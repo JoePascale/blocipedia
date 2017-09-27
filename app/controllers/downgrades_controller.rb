1390220b39cca2396d618c302b34a5a95cfc52af
JoePascale <joe.m.pascale@gmail.com>
2017-09-27 15:43:11 -0700

    altered private wiki behavior dependent upon standard or premium users

--- app/controllers/downgrades_controller.rb
+++ app/controllers/downgrades_controller.rb
@@ -1,7 +1,7 @@
 class DowngradesController < ApplicationController
   def create
     current_user.standard!
-    #current_user.wikis.public!
+    current_user.wikis.update_all(:private => false )
     redirect_to root_path
   end
 end
